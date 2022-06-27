c8e36244999cdfa90bb82713e567662def46b7b6
Agrit Tiwari <agrit1408@gmail.com>
2022-06-27 10:12:54 +0530

    Detailed Invoice component added

--- my-app/Components/Invoice/Invoice.jsx
+++ my-app/Components/Invoice/Invoice.jsx
@@ -1,19 +1,34 @@
 // div for the total Invoice
-
-
 import React from 'react'
+import useSWR from 'swr'
+import fetcher from '../../lib/fetcher'
 import Style from './Invoice.module.css'
 const Invoice = (props) => {
-  const {_id,  name,  dueDate, billNo,  billDate,  lineItem,  grossAmount,  gstAmount,  netAmount,  notes,  status,  createdAt,  updatedAt } =props.invoice;
+  const {selectedInvoice} = props
+  
+  const {data, error} = useSWR('/api',fetcher) 
+    
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+  
+    // render data
+    const {invoiceData} = data;
+    console.log(invoiceData.invoices)
+    const selectedBill = invoiceData.invoices.find(bill=> bill._id ===selectedInvoice);
+    console.log(selectedBill)
+    
+    
+    // const {  name,  dueDate, billNo,  billDate,  lineItem,  grossAmount,  gstAmount,  netAmount,  notes,  status,  createdAt,  updatedAt } = selectedBill
     return (
     <div className={Style.invoice}>
         <div className={Style.div1}>
 <strong>Name</strong><span>{name}</span>
         </div>
         <div className={Style.div2}>
-           
+          Invoice Bill Component
+           {/* <h2>{name}</h2>
                 {lineItem.map(item => <Item key={item._id} item={item}/>)}
-            
+             */}
 
         </div>
     </div>
--- my-app/Components/List/ListItem.jsx
+++ my-app/Components/List/ListItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
+import Invoice from '../Invoice/Invoice'
 import Style from './List.module.css'
 
 export const ListItem = (props) => {
 const {invoice} = props
-console.log(invoice)
+const [selected, setSelected] = useState({_id:null,show: false
+})
+// console.log(invoice)
+// console.log(selected)
   return (
   <div>
     <ul className={Style.row}>
@@ -13,8 +18,9 @@ console.log(invoice)
         <li>{invoice.billDate.slice(0, 10)}</li>
         <li>{invoice.netAmount}</li>
         <li>{invoice.status}</li>
-        <li><button>Show this bill </button></li>
-    </ul>    
+        <li><button onClick={()=> setSelected({_id:invoice._id, show:!selected.show})}>Show this bill </button></li>
+    </ul>  
+    {selected.show && <Invoice selectedInvoice={selected._id}/>}  
 </div>)
 }
 
